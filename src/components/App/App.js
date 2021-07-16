38c2d594eed8c17cd03a2966c7492e23754a124d
Viktor <chelweb18@gmail.com>
2021-07-16 17:11:48 +0500

    Edit style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -45,7 +45,7 @@ function App(props) {
 
   return (
     <div className="App">
-      <h1 className="App__title">Сообщения наших пользователей</h1>
+      <h1 className="App__title">Введите любое сообщение, робот вам ответит в течение 2 секунд</h1>
       <div className="message">
 
           { messageList.map((el, index) => {
